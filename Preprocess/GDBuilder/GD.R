bacffd38dadcb7785048facad1901b4e23e08217
srkruger <washier@gmail.com>
2015-05-28 20:12:18 +0200

    Adding Google Distance stuff

---
+++ Preprocess/GDBuilder/GD.R
@@ -0,0 +1,160 @@
+require(readr)
+require(stringr)
+
+#Use scrubbed data
+train <- read_csv("../../Processed/train_scrubbed.csv")
+test <- read_csv("../../Processed/test_scrubbed.csv")
+
+#Build corpora and write 'em to file
+A <- unique(c(train$product_title, train$product_description, test$product_title, test$product_description))
+B <- c(train$query, test$query)
+C <- c(train$product_title, test$product_title)
+fileConn <- file("A.txt")
+writeLines(A, fileConn)
+close(fileConn)
+fileConn <- file("B.txt")
+writeLines(B, fileConn)
+close(fileConn)
+fileConn <- file("C.txt")
+writeLines(C, fileConn)
+close(fileConn)
+
+### Run GDBuilder C# Code ###
+
+#Load the dictionary
+dict <- as.data.frame(read_csv("dict.csv"))
+#Apply minimum count to reduce the size quite a bit
+MIN_COUNT = 20
+dict <- dict[dict$co >= MIN_COUNT, ]
+#Apply min word length
+MIN_WORD_LEN = 2
+dict <- dict[str_length(dict$word1) >= MIN_WORD_LEN & 
+                 (str_length(dict$word2) >= MIN_WORD_LEN | dict$word2 == ""), ]
+
+#Read the Distances
+fileConn <- file("distances.txt")
+distances <- as.numeric(readLines(fileConn))
+close(fileConn)
+#and plot against target
+plot(factor(train$median_relevance), distances[1:nrow(train)])
+
+#See http://en.wikipedia.org/wiki/Normalized_Google_distance
+#Uses the dictionary produced by the GDBuilder
+GoogleDistance <- function(dictionary, word1, word2, size)
+{
+    gd <- Inf
+    word12 <- sort(c(word1, word2), decreasing = FALSE)
+    word1 <- word12[1]
+    word2 <- word12[2]
+    if(sum(dictionary$word1 == word1 & dictionary$word2 == "") == 1 &
+           sum(dictionary$word1 == word2 & dictionary$word2 == "") == 1 &
+           sum(dictionary$word1 == word1 & dictionary$word2 == word2) == 1)
+    {
+        lw1 <- log(dictionary[dictionary$word1 == word1 & dictionary$word2 == "", "co"])
+        lw2 <- log(dictionary[dictionary$word1 == word2 & dictionary$word2 == "", "co"])
+        lw12 <- log(dictionary[dictionary$word1 == word1 & dictionary$word2 == word2, "co"])
+        gd <- (max(lw1, lw2) - lw12) / (log(size) - min(lw1, lw2))
+    }
+    gd
+}
+
+GoogleDistance(dict, "accent", "pillow", length(A))
+GoogleDistance(dict, "samsung", "phone", length(A))
+GoogleDistance(dict, "appl", "phone", length(A))
+GoogleDistance(dict, "iphon", "appl", length(A))
+GoogleDistance(dict, "ipad", "appl", length(A))
+GoogleDistance(dict, "nexus", "googl", length(A))
+GoogleDistance(dict, "white", "appl", length(A))
+GoogleDistance(dict, "white", "samsung", length(A))
+GoogleDistance(dict, "kitchen", "style", length(A))
+GoogleDistance(dict, "kitchen", "electric", length(A))
+GoogleDistance(dict, "kitchen", "steel", length(A))
+GoogleDistance(dict, "kitchen", "counter", length(A))
+
+#See http://en.wikipedia.org/wiki/Normalized_Google_distance
+#Uses the dictionary produced by the GDBuilder
+FindClosestWords <- function(dictionary, word, size, limit = 0)
+{
+    if(sum(dictionary$word1 == word) == 0 | sum(dictionary$word2 == word) == 0)
+        return(NULL)
+    
+    occursw1 <- dictionary$word1 == word & dictionary$word2 != ""
+    occursw2 <- dictionary$word2 == word & dictionary$word1 != ""
+    relatedWords <- c(dictionary$word2[occursw1], dictionary$word1[occursw2])
+    if(limit > 0)  
+        relatedWords <- head(relatedWords[order(-c(dictionary$co[occursw1], dictionary$co[occursw2]))], limit)
+    
+    distances <- sapply(relatedWords, FUN = GoogleDistance, dictionary = dict, word2 = word, size = size)
+    relatedWords <- data.frame(word = relatedWords, distance = distances)
+    relatedWords <- relatedWords[order(relatedWords$distance), ]
+    relatedWords
+}
+
+FindClosestWords(dict, "samsung", length(A), 10)
+FindClosestWords(dict, "appl", length(A), 10)
+FindClosestWords(dict, "kitchen", length(A), 10)
+FindClosestWords(dict, "number", length(A), 10)
+FindClosestWords(dict, "googl", length(A), 10)
+FindClosestWords(dict, "electric", length(A), 10)
+FindClosestWords(dict, "brand", length(A), 10)
+FindClosestWords(dict, "wifi", length(A), 10)
+
+#Uses the dictionary produced by the GDBuilder
+#Not working well
+PhraseDistance <- function(dictionary, phrase1, phrase2, size)
+{
+    words1 <- unlist(str_split(phrase1, pattern="[[:space:]]"))
+    words2 <- unlist(str_split(phrase2, pattern="[[:space:]]"))
+    
+    numDistances = 0
+    sumDistances = 0
+    for(i in 1:length(words1))
+    {
+        w1 <- words1[i]
+        if(w1 == "")
+            next
+        
+        for(j in 1:length(words2))
+        {
+            w2 <- words2[j]
+            if(w2 == "")
+                next
+            
+            if(w1 != w2)
+            {
+                
+                d = GoogleDistance(dictionary, w1, w2, size)
+                if(d != Inf)
+                {
+                    numDistances = numDistances + 1
+                    sumDistances = sumDistances + d
+                }
+            }
+        }
+    }
+    if(numDistances == 0)
+        return(Inf)
+    if(sumDistances == 0)
+        return(0)
+    
+    sumDistances / numDistances
+}
+
+PhraseDistance(dict, "cell phon", "samsung galaxi not", length(A))
+PhraseDistance(dict, "cell phon", "wifi router", length(allText))
+PhraseDistance(dict, "cell phon", "wireless number modem", length(A))
+PhraseDistance(dict, "playstation games bundl", "soni", length(A))
+PhraseDistance(dict, "console game", "nintendo wii", length(A))
+PhraseDistance(dict, "boyfriend jean", "how make american quilt (dvd)", length(A))
+PhraseDistance(dict, "boyfriend jean", "levi", length(A))
+PhraseDistance(dict, "boyfriend jean", "zipper levi style", length(A))
+
+
+plot(distances[1:nrow(train)], train$median_relevance)
+
+fileConn <- file("train_dist_q2t.txt")
+writeLines(as.character(dist_q2t), fileConn)
+close(fileConn)
+
+
+
---
+++ Preprocess/GDBuilder/GDBuilder/Program.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace GDBuilder
+{
+    class Program
+    {
+        private static readonly char[] SPACE_DELIM = new char[] { ' ' };
+
+        static void Main(string[] args)
+        {
+            if (args.Length < 5)
+            {
+                PrintUsage();
+                return;
+            }
+
+            //Read the corpora
+            List<string> A = ReadInput(args[0]);
+            Console.WriteLine(string.Format("READ A. {0} DOCUMENTS", A.Count));
+            List<string> B = ReadInput(args[1]);
+            Console.WriteLine(string.Format("READ B. {0} DOCUMENTS", B.Count));
+            List<string> C = ReadInput(args[2]);
+            Console.WriteLine(string.Format("READ C. {0} DOCUMENTS", C.Count));
+
+            //Output the co-occurence dictionary, saving it to calc the distances
+            Console.Write("BUILDING DICT...");
+            Dictionary<string, Dictionary<string, int>> dict = OutputDict(A, args[4]);
+            Console.WriteLine("DONE");
+
+            //Output the distances
+            Console.Write("BUILDING DICT...");
+            OutputDistances(dict, args[3], B, C, A.Count);
+            Console.WriteLine("DONE");
+        }
+
+        private static void PrintUsage()
+        {
+            Console.WriteLine("GDBuilder");
+            Console.WriteLine("Turns 3 corpora, A, B & C into a word-word co-occurrence");
+            Console.WriteLine("dictionary for A, and a set of distances");
+            Console.WriteLine("between pairs of documents in B and C.");
+            Console.WriteLine("B & C should be of the same length.");
+            Console.WriteLine();
+            Console.WriteLine("The co-occurrence dictionary is ouput in CSV format with header: ");
+            Console.WriteLine("word1,word2,co");
+            Console.WriteLine();
+            Console.WriteLine("where word1 and word2 are two different words");
+            Console.WriteLine("in alphabetical order and co is the");
+            Console.WriteLine("number of documents in A where these");
+            Console.WriteLine("words co-occurr. A blank word2 signifies co is the number of");
+            Console.WriteLine("documents that contain word1.");
+            Console.WriteLine();
+            Console.WriteLine("The distances between documents in B & C are output");
+            Console.WriteLine("as a text file, one distance per document pair.");
+            Console.WriteLine();
+            Console.WriteLine("Usage : GDBuilder a b c dist dict");
+            Console.WriteLine();
+            Console.WriteLine("where a, b and c are the paths to the corpora A, B & C");
+            Console.WriteLine("respectively. Corpora should be text files with");
+            Console.WriteLine("a single line of text per document(no newlines and\\or carriage returns).");
+            Console.WriteLine("dist and dict are the paths to use for output of the distances and");
+            Console.WriteLine("dictionary respectively");
+
+
+        }
+
+        private static double GoogleDistance(Dictionary<string, Dictionary<string, int>> dict,
+            string word1, string word2, int size)
+        {
+            if (!dict.ContainsKey(word1) || !dict.ContainsKey(word2) || !dict[word1].ContainsKey(word2))
+                return double.PositiveInfinity;
+
+            double lw1 = Math.Log(dict[word1][string.Empty]);
+            double lw2 = Math.Log(dict[word2][string.Empty]);
+            double lw12 = Math.Log(dict[word1][word2]);
+            return (Math.Max(lw1, lw2) - lw12) / (Math.Log(size) - Math.Min(lw1, lw2));
+        }
+
+        private static void OutputDistances(Dictionary<string, Dictionary<string, int>> dict,
+            string path, List<string> corpus1, List<string> corpus2, int corpusSize)
+        {
+            using (StreamWriter sw = File.CreateText(path))
+            {
+                for (int i = 0; i < corpus1.Count; i++)
+                {
+                    string[] words1 = corpus1[i].Split(SPACE_DELIM, StringSplitOptions.RemoveEmptyEntries);
+                    string[] words2 = corpus2[i].Split(SPACE_DELIM, StringSplitOptions.RemoveEmptyEntries);
+
+                    double numD = 0;
+                    double sumD = 0;
+                    double phraseD = 0;
+                    for (int w = 0; w < words1.Length; w++)
+                    {
+                        string w1 = words1[w];
+
+                        for (int j = 0; j < words2.Length; j++)
+                        {
+                            string w2 = words2[j];
+                            double d = GoogleDistance(dict, w1, w2, corpusSize);
+                            if (!double.IsPositiveInfinity(d))
+                            {
+                                numD++;
+                                sumD += d;
+                            }
+                        }
+                    }
+                    if (numD == 0)
+                        phraseD = double.PositiveInfinity;
+                    else if (sumD == 0)
+                        phraseD = 0;
+                    else
+                        phraseD = sumD / numD;
+
+                    if (double.IsPositiveInfinity(phraseD))
+                        sw.WriteLine("Inf");
+                    else
+                        sw.WriteLine(phraseD);
+                }
+            }
+        }
+
+        private static Dictionary<string, Dictionary<string, int>> OutputDict(List<string> corpus, string path)
+        {
+            Dictionary<string, Dictionary<string, int>> result =
+                new Dictionary<string, Dictionary<string, int>>(corpus.Count * 100);
+            foreach (string phrase in corpus)
+            {
+                List<string> tokens = new List<string>(phrase.Split(SPACE_DELIM, StringSplitOptions.RemoveEmptyEntries));
+                List<string> uniqueWords = new List<string>(tokens.Count);
+                foreach (string word in tokens)
+                {
+                    if (!uniqueWords.Contains(word))
+                        uniqueWords.Add(word);
+                }
+
+                //Count word ocurrences
+                foreach (string word in uniqueWords)
+                {
+                    if (!result.ContainsKey(word))
+                        result.Add(word, new Dictionary<string, int>());
+                    if (!result[word].ContainsKey(string.Empty))
+                        result[word].Add(string.Empty, 1);
+                    else
+                        result[word][string.Empty] = result[word][string.Empty] + 1;
+                }
+
+                //Count co-ocurrences
+                for (int i = 0; i < uniqueWords.Count - 1; i++)
+                {
+                    string word1 = uniqueWords[i];
+                    for (int j = i + 1; j < uniqueWords.Count; j++)
+                    {
+                        string word2 = uniqueWords[j];
+
+                        if (!result[word1].ContainsKey(word2))
+                            result[word1].Add(word2, 1);
+                        else
+                            result[word1][word2] = result[word1][word2] + 1;
+
+                        if (!result[word2].ContainsKey(word1))
+                            result[word2].Add(word1, 1);
+                        else
+                            result[word2][word1] = result[word2][word1] + 1;
+                    }
+                }
+            }
+
+            using (StreamWriter sw = File.CreateText(path))
+            {
+                Dictionary<string, bool> unique = new Dictionary<string, bool>(result.Keys.Count / 2);
+                sw.WriteLine("word1,word2,co");
+                List<string> wordPair = new List<string>() { string.Empty, string.Empty };
+                foreach (KeyValuePair<string, Dictionary<string, int>> entry in result)
+                {
+                    foreach (KeyValuePair<string, int> word2 in entry.Value)
+                    {
+                        wordPair[0] = entry.Key;
+                        wordPair[1] = word2.Key;
+                        if (word2.Key != string.Empty)
+                            wordPair.Sort();
+                        string key = wordPair[0] + " " + wordPair[1];
+                        if (!unique.ContainsKey(key))
+                        {
+                            unique.Add(key, true);
+                            sw.Write(wordPair[0]);
+                            sw.Write(',');
+                            if (word2.Key != string.Empty)
+                                sw.Write(wordPair[1]);
+                            sw.Write(',');
+                            sw.WriteLine(word2.Value);
+                        }
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        private static List<string> ReadInput(string path)
+        {
+            List<string> result = new List<string>();
+            string line = null;
+            using (StreamReader sr = File.OpenText(path))
+            {
+                while ((line = sr.ReadLine()) != null)
+                    result.Add(line);
+            }
+
+            return result;
+        }
+    }
+}
