6b884db7cf4ad4ae684fc19d5814b637aba3b3fc
Frans Slothouber <rfsber@xs4all.nl>
2015-06-17 19:02:42 +0200

    0.62497

--- Preprocess/scrub.r
+++ Preprocess/scrub.r
@@ -16,42 +16,41 @@ scrub_data <- function(doc_vector) {
     gg <- gsub("\\s", " ", doc_vector)
     gg <- tolower(gg)
     if (0) {
-	    # This takes care of most of the CSS
-	    gg <- gsub("\\.[a-z]+\\s", " ", gg)
-	    gg <- gsub("\\b[a-z]+\\.[a-z]+\\b", " ", gg)
-	    gg <- gsub("\\b[a-z-]+:[^;]+;", " ", gg)
-	    gg <- gsub("\\b(li|ul|hr|h1|h2|h3|h4|h5|body|p|div|table)\\b", " ", gg)
-	    gg <- gsub("'", "", gg)
-	    gg <- gsub(":", " ", gg)
-	    gg <- gsub("\\.", " ", gg)
-	    # This takes care of most of the HTML
-	    gg <- gsub("<[a-z]+>", "", gg)
-	    gg <- gsub("</[a-z]+>", "", gg)
-
+        # This takes care of most of the CSS
+        gg <- gsub("\\.[a-z]+\\s", " ", gg)
+        gg <- gsub("\\b[a-z]+\\.[a-z]+\\b", " ", gg)
+        gg <- gsub("\\b[a-z-]+:[^;]+;", " ", gg)
+        gg <- gsub("\\b(li|ul|hr|h1|h2|h3|h4|h5|body|p|div|table)\\b", " ", gg)
+        gg <- gsub("'", "", gg)
+        gg <- gsub(":", " ", gg)
+        gg <- gsub("\\.", " ", gg)
+        # This takes care of most of the HTML
+        gg <- gsub("<[a-z]+>", "", gg)
+        gg <- gsub("</[a-z]+>", "", gg)
     }
     # Remove all unicode and other nonsense
     gg <- gsub("[^a-z 0-9-]", "#", gg)
     if (0) {
-	    # Remove any words that dare to have funny characters...
-	    gg <- gsub("\\S*#\\S*", "", gg)
-	    # Replace numbers with the word number
-	    #gg <- gsub("\\b\\d+\\b", "number", gg)
-
-	    if (0) {
-		# All this is done in the pipeline of the model.
-		corpus <- VCorpus(VectorSource(gg))
-		# High freq. words such as "a", "the", etc removed
-		corpus <- tm_map(corpus, removeWords, stopwords("english"))
-		# Sandals -> Sandal,  memory -> memori
-		corpus <- tm_map(corpus, stemDocument)
-		# Remove unnecessary spacing
-		# corpus <- tm_map(corpus, stripWhitespace)
-		gg <- vapply(corpus, as.character, FUN.VALUE="", USE.NAMES=FALSE)
-	    }
-	    gg <- gsub("-", "", gg)
-	    gg <- gsub("\\s+", " ", gg)
-	    # Remove possible space at the begin of a sentence.
+        # Remove any words that dare to have funny characters...
+        gg <- gsub("\\S*#\\S*", "", gg)
+        # Replace numbers with the word number
+        #gg <- gsub("\\b\\d+\\b", "number", gg)
+        if (0) {
+            # All this is done in the pipeline of the model.
+            corpus <- VCorpus(VectorSource(gg))
+            # High freq. words such as "a", "the", etc removed
+            corpus <- tm_map(corpus, removeWords, stopwords("english"))
+            # Sandals -> Sandal,  memory -> memori
+            corpus <- tm_map(corpus, stemDocument)
+            # Remove unnecessary spacing
+            # corpus <- tm_map(corpus, stripWhitespace)
+            gg <- vapply(corpus, as.character, FUN.VALUE="", USE.NAMES=FALSE)
+        }
+        gg <- gsub("-", "", gg)
     }
+    gg <- gsub("#", "-", gg)
+    gg <- gsub("\\s+", " ", gg)
+    # Remove possible space at the begin of a sentence.
     gg <- gsub("^\\s", "", gg)
     return(gg)
 }
