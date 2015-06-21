1634fa9ce2677116ab2106c53832ecdeac40f7e0
thomas.jungblut <thomas.jungblut@gmail.com>
2015-06-21 14:34:51 +0100

    lb 0.66, distances + rudis 0.59 model

--- Models/BTBAveraged/BeatTheBenchmark.py
+++ Models/BTBAveraged/BeatTheBenchmark.py
@@ -1,6 +1,6 @@
 
 """
-Beating the Benchmark 
+Beating the Benchmark
 Search Results Relevance @ Kaggle
 __author__ : Abhishek
 
@@ -13,17 +13,30 @@ from scipy import sparse
 from operator import itemgetter
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.svm import SVC
-from sklearn.base import BaseEstimator
+from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.cross_validation import StratifiedKFold
 from sklearn.grid_search import RandomizedSearchCV
 from sklearn.decomposition import TruncatedSVD
 from sklearn.preprocessing import StandardScaler
 from sklearn import decomposition, pipeline, metrics, grid_search
-from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import SelectKBest,SelectPercentile
+from sklearn.feature_selection import chi2
 from sklearn.pipeline import Pipeline, FeatureUnion
 from nltk import word_tokenize
 from nltk.stem import WordNetLemmatizer
 from nltk.stem.porter import PorterStemmer
+#====================================================================
+
+
+class ItemSelector(BaseEstimator, TransformerMixin):
+    def __init__(self, key):
+        self.key = key
+
+    def fit(self, x, y=None):
+        return self
+
+    def transform(self, data_dict):
+        return data_dict[self.key]
 
 class LemmaTokenizer(object):
     def __init__(self):
@@ -35,7 +48,6 @@ class LemmaTokenizer(object):
         # as a list of words.
         return [self.wnl.lemmatize(t) for t in word_tokenize(doc)]
 
-        
 # The following 3 functions have been taken from Ben Hamner's github repository
 # https://github.com/benhamner/Metrics
 def confusion_matrix(rater_a, rater_b, min_rating=None, max_rating=None):
@@ -124,28 +136,38 @@ def quadratic_weighted_kappa(y, y_pred):
     return (1.0 - numerator / denominator)
 
 
-def build_stacked_model():
+def build_stacked_model(dist_colnames):
     countvect_char = TfidfVectorizer(
             tokenizer = LemmaTokenizer(),
-            analyzer="char_wb", 
-            binary=False, 
+            analyzer="char_wb",
+            binary=False,
             norm = None,
             stop_words = 'english')
 
     svd = TruncatedSVD()
-    
-    combined_features = FeatureUnion([("svd", svd)])
-    
-    scl = StandardScaler(with_mean=False)
+    scl = StandardScaler()
     clf = SVC()
 
     p = Pipeline([
-        ('vect', countvect_char),
-        ('union', combined_features),
+        # Use FeatureUnion to combine the features
+        # query and title, and distance metrics
+        ('features', FeatureUnion(
+            transformer_list=[
+                ('cvt', Pipeline([
+                    ('selectordoc', ItemSelector(key='doc')),
+                    ('vect', countvect_char),
+                    ('svd', svd),
+                ])),
+                ('dist', Pipeline([
+                    ('selectordist', ItemSelector(key=dist_colnames)),
+                    ('distselect', SelectKBest(chi2))
+                ]))
+            ]
+        )),
+        # Use a SVC classifier on the combined features
         ('scl', scl),
-        ('clf', clf)]
-    )
-
+        ('clf', clf),
+    ])
     return p
 
 def report(grid_scores, n_top=5):
@@ -157,30 +179,32 @@ def report(grid_scores, n_top=5):
               np.std(score.cv_validation_scores)))
         print("Parameters: {0}".format(score.parameters))
         print("")
-        
+
 def frange(x, y, jump):
     while x < y:
         yield x
-        x += jump        
+        x += jump
 
-def grid_search_parameters():
-    # Create the pipeline 
-    clf = build_stacked_model()
+def combine_factors(x):
+    s = "{} {}".format(x['query'], x['product_title'])
+    return s
 
-    # Create a parameter grid to search for 
+def grid_search_parameters(traindata, y, dist_colnames):
+    # Create the pipeline
+    clf = build_stacked_model(dist_colnames)
+
+    # Create a parameter grid to search for
     # best parameters for everything in the pipeline
     param_grid = {
-            'vect__ngram_range' : [(1, 4), (1, 5), (1, 6)],
-            'vect__min_df' : list(range(3, 10, 1)),
-            'union__svd__n_components' : list(range(100, 250, 5)),
+            'features__cvt__vect__ngram_range' : [(1, 4), (1, 5), (1, 6)],
+            'features__cvt__vect__min_df' : list(range(3, 10, 1)),
+            'features__cvt__svd__n_components' : list(range(100, 250, 5)),
+            'features__dist__distselect__k' : list(range(10, len(dist_colnames), 5)),
             'clf__degree' : list(range(2, 6, 1)),
             'clf__C' : list(range(4, 10, 1)),
-            #'clf__C' : [8],
-            #'clf__degree' : [4],
-            #'svd__n_components' : [180],
         }
 
-    # Kappa Scorer 
+    # Kappa Scorer
     kappa_scorer = metrics.make_scorer(quadratic_weighted_kappa, greater_is_better = True)
 
     # Cross validation
@@ -199,42 +223,36 @@ def grid_search_parameters():
             iid=True,
             refit=False)
 
-
     # Fit Grid Search Model
     print("Fitting training data .")
     model.fit(traindata, y)
 
-    report(model.grid_scores_)    
+    report(model.grid_scores_)
 
-def submit(traindata, y, testdata, idx):
+def submit(traindata, y, testdata, idx, dist_colnames):
     params = []
     params.append({
-            'vect__ngram_range' : (1, 5),
-            'vect__min_df' : 3,
-            'svd__n_components' : 235,
-            'clf__degree' : 2,
-            'clf__C' : 6,
-        })   
-    params.append({
-            'vect__ngram_range' : (1, 5),
-            'vect__min_df' : 6,
-            'svd__n_components' : 195,
-            'clf__degree' : 2,
+            #0.655 cv score
+            'features__cvt__vect__ngram_range' : (1, 6),
+            'features__cvt__vect__min_df' : 3,
+            'features__cvt__svd__n_components' : 200,
+            'features__dist__distselect__k' : 30,
+            'clf__degree' : 5,
             'clf__C' : 9,
-        }) 
-    
+        })
+
     predictions = []
     for param in params:
         print("Fitting: {0}".format(params))
-        clf = build_stacked_model()
+        clf = build_stacked_model(dist_colnames)
         clf.set_params(**param)
         clf.fit(traindata, y)
         predictions.append(clf.predict(testdata))
-    
-    # add rudis predictions here too    
-    rudisPreds = pd.read_csv('../srkruger1_59719_submit.csv')
+
+    # add rudis predictions here too
+    rudisPreds = pd.read_csv('../srkruger1_61897_submit.csv')
     predictions.append(rudisPreds['prediction'].tolist())
-    
+
     print("Creating submission")
     num_examples = len(testdata)
     num_models = len(predictions)
@@ -244,16 +262,18 @@ def submit(traindata, y, testdata, idx):
         for j in range(num_models):
             sum += predictions[j][i]
         preds.append(int(math.floor(sum / num_models)))
-        
+
     submission = pd.DataFrame({"id": idx, "prediction": preds})
     submission.to_csv("Submission/sub.csv", index=False)
     print("done")
 
 if __name__ == '__main__':
     # Load the training file
-    print("Loading data.")    
+    print("Loading data.")
     train = pd.read_csv('../../Raw/train.csv')
+    train_dist = pd.read_csv('../../Processed/distances_features.csv')
     test = pd.read_csv('../../Raw/test.csv')
+    test_dist = pd.read_csv('../../Processed/distances_features_test.csv')
 
     print("Preprocessing")
     # Make of copy of the query ids so we
@@ -261,20 +281,27 @@ if __name__ == '__main__':
     idx = test.id.values.astype(int)
     # we dont need ID columns
     train = train.drop('id', axis=1)
+    train_dist = train_dist.drop('id', axis=1)
     test  = test.drop('id', axis=1)
+    test_dist = test_dist.drop('id', axis=1)
 
     # create labels. drop useless columns
     y = train.median_relevance.values
     train = train.drop(['median_relevance', 'relevance_variance'], axis=1)
+    train_dist = train_dist.drop(['outcome'], axis=1)
 
-    # do some lambda magic on text columns
-    # Combine query and product_title into one string
-    traindata = list(train.apply(lambda x:'%s %s' % 
-        (x['query'],x['product_title']),axis=1))
-    testdata = list(test.apply(lambda x:'%s %s' % 
-        (x['query'],x['product_title']),axis=1))
+    dist_colnames = list(train_dist.columns.values)
 
-    grid_search_parameters()
-    
-    #submit(traindata, y, testdata, idx)   
+    trainX = pd.DataFrame({
+                'doc':list(train.apply(combine_factors, axis=1)),
+             })
+    trainX = pd.concat([trainX, train_dist], axis = 1, join_axes=[trainX.index])
+    testX =  pd.DataFrame({
+                'doc':list(test.apply(combine_factors, axis=1))
+             })
+    testX = pd.concat([testX, test_dist], axis = 1, join_axes=[testX.index])
+
+    #grid_search_parameters(trainX, y, dist_colnames)
+
+    submit(trainX, y, testX, idx, dist_colnames)
 
